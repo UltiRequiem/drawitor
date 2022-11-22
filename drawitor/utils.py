0a848a388d9f00e7215cc7fe6cf7d71df7cb4e68
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-11-22 15:40:25 -0500

    Update utils.py

--- drawitor/utils.py
+++ drawitor/utils.py
@@ -7,5 +7,7 @@ def colored(red: int, green: int, blue: int, text: str) -> str:
     return f"\033[38;2;{red};{green};{blue}m{text}"
 
 
+clear_command = "clear" if os.name == "posix" else "cls"
+
 def clear_console():
-    os.system("clear" if os.name == "posix" else "cls")
+    os.system(clear_command)
